7237b4867fbd29635f5b14ecb3da0f94be70e650
Hariadi Hinta <diperakui@yahoo.com>
2014-04-21 08:42:57 +0800

    sectors model, route and view

---
+++ app/routes/admin/sectors.php
@@ -0,0 +1,131 @@
+<?php
+
+Route::collection(array('before' => 'auth,csrf'), function() {
+
+	/*
+		List Sectors
+	*/
+	Route::get(array('admin/sectors', 'admin/sectors/(:num)'), function($page = 1) {
+		$vars['messages'] = Notify::read();
+		$vars['sectors'] = Sector::paginate($page, Config::get('meta.posts_per_page'));
+		$vars['hierarchies'] = Config::app('hierarchy');		
+
+		return View::create('sectors/index', $vars)
+			->partial('header', 'partials/header')
+			->partial('footer', 'partials/footer');
+	});
+
+	/*
+		Edit Sector
+	*/
+	Route::get('admin/sectors/edit/(:num)', function($id) {
+		$vars['messages'] = Notify::read();
+		$vars['token'] = Csrf::token();
+		$vars['sector'] = Sector::find($id);
+
+		return View::create('sectors/edit', $vars)
+			->partial('header', 'partials/header')
+			->partial('footer', 'partials/footer');
+	});
+
+	Route::post('admin/sectors/edit/(:num)', function($id) {
+		$input = Input::get(array('title', 'slug', 'description'));
+
+		if(empty($input['slug'])) {
+			$input['slug'] = acronym($input['title']);
+		}
+
+		$input['slug'] = slug($input['slug']);
+
+		$validator = new Validator($input);
+
+		$validator->check('title')
+			->is_max(3, __('hierarchy.title_missing'));
+
+		$validator->add('duplicate', function($str) use($id) {
+			return Sector::where('slug', '=', $str)->where('id', '<>', $id)->count() == 0;
+		});
+
+		$validator->check('slug')
+			->is_max(3, __('posts.slug_missing'))
+			->is_duplicate(__('posts.slug_duplicate'));
+
+		if($errors = $validator->errors()) {
+			Input::flash();
+
+			Notify::warning($errors);
+
+			return Response::redirect('admin/sectors/edit/' . $id);
+		}
+
+		Sector::update($id, $input);
+
+		Notify::success(__('hierarchy.updated'));
+
+		return Response::redirect('admin/sectors/edit/' . $id);
+	});
+
+	/*
+		Add sector
+	*/
+	Route::get('admin/sectors/add', function() {
+		$vars['messages'] = Notify::read();
+		$vars['token'] = Csrf::token();
+
+		return View::create('sectors/add', $vars)
+			->partial('header', 'partials/header')
+			->partial('footer', 'partials/footer');
+	});
+
+	Route::post('admin/sectors/add', function() {
+		$input = Input::get(array('title', 'slug', 'description'));
+
+		if(empty($input['slug'])) {
+			$input['slug'] = acronym($input['title']);
+		}
+
+		$input['slug'] = slug($input['slug']);
+
+		$validator = new Validator($input);
+
+		$validator->check('title')
+			->is_max(3, __('hierarchy.title_missing'));
+
+		$validator->add('duplicate', function($str) {
+			return Branch::where('slug', '=', $str)->count() == 0;
+		});
+
+		$validator->check('slug')
+			->is_max(3, __('posts.slug_missing'))
+			->is_duplicate(__('posts.slug_duplicate'));
+
+		if($errors = $validator->errors()) {
+			Input::flash();
+
+			Notify::warning($errors);
+
+			return Response::redirect('admin/sectors/add');
+		}
+
+		Sector::create($input);
+
+		Notify::success(__('hierarchy.created'));
+
+		return Response::redirect('admin/sectors');
+	});
+
+	/*
+		Delete sector
+	*/
+	Route::get('admin/sectors/delete/(:num)', function($id) {
+		Sector::find($id)->delete();
+
+		//TODO: admin only, not for PTB
+		Hierarchy::where('sector', '=', $id)->update(array('sector' => 0));
+
+		Notify::success(__('hierarchy.deleted'));
+
+		return Response::redirect('admin/sectors');
+	});
+
+});
---
+++ app/views/sectors/index.php
@@ -0,0 +1,67 @@
+<?php echo $header; ?>
+
+<?php echo Html::link('admin/sectors/add', __('hierarchy.create_sector'), array('class' => 'btn btn-primary pull-right')); ?>
+
+<h1 class="page-header"><?php echo __('hierarchy.sector'); ?></h1>
+
+<?php echo $messages; ?>
+
+<div class="row">
+	<div class="col-lg-9">
+		<?php //print_r($sectors); ?>
+		<div class="table-responsive">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th><?php echo __('hierarchy.sector'); ?></th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php if($sectors->count): ?>
+					<?php foreach($sectors->results as $sector): 
+					$counter = ($sectors->page - 1) * Config::meta('posts_per_page')
+					?>
+					<tr class="status draft">
+						<td><?php echo $counter+1; ?></td>
+						<td><a href="<?php echo Uri::to('admin/sectors/edit/' . $sector->id); ?>"><?php echo $sector->title; ?></a></td>
+					</tr>
+				<?php endforeach; ?>
+				<?php else: ?>
+                <tr>
+                  <td colspan="7"><?php echo __('hierarchy.no_reports'); ?></td>
+                </tr>
+				<?php endif; ?>
+			</tbody>
+		</table>
+
+		<?php if ($sectors->links()) : ?>
+		<ul class="pagination">
+			<?php echo $sectors->links(); ?>
+		</ul>
+		<?php endif; ?>
+		
+		</div>
+	</div>
+	<div class="col col-lg-3">
+		<nav class="list-sector sidebar">
+
+			<div class="panel panel-default">
+				<div class="panel-heading">Related</div>
+				<div class="list-group">
+					<?php foreach($hierarchies as $key => $hierarchy): ?>
+
+						<?php if ( $key.'s' !== basename(Uri::current()) ) : ?>
+					
+						<?php echo Html::link('admin/' . $key . 's', __('hierarchy.' . $key), array('class' => 'list-group-item'
+						)); ?>
+
+						<?php endif; ?>
+					<?php endforeach; ?>
+				</div>
+			</div>
+		</nav>
+	</div>
+</div>
+
+<?php echo $footer; ?>
