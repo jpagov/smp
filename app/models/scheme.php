ca6f9a350296847d2065214ca4b8816eab2affc1
Hariadi Hinta <diperakui@yahoo.com>
2014-04-22 01:22:39 +0800

    add scheme model and data

---
+++ app/models/scheme.php
@@ -0,0 +1,31 @@
+<?php
+
+class Scheme extends Base {
+
+	public static $table = 'schemes';
+
+	public static function dropdown() {
+		$items = array();
+
+		foreach(static::get() as $item) {
+			$items[strtoupper($item->slug)] = $item->title;
+		}
+
+		return $items;
+	}
+
+	public static function slug($slug) {
+		return static::where('slug', 'like', $slug)->fetch();
+	}
+
+	public static function paginate($page = 1, $perpage = 10) {
+		$query = Query::table(static::table());
+
+		$count = $query->count();
+
+		$results = $query->take($perpage)->skip(($page - 1) * $perpage)->sort('title')->get();
+
+		return new Paginator($results, $count, $page, $perpage, Uri::to('schemes'));
+	}
+
+}
