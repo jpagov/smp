463ed0782a3fd2c1e057352fbbdec29844bc55d6
Hariadi Hinta <diperakui@yahoo.com>
2014-07-22 11:26:45 +0800

    filter json api with division

--- app/routes/public/api.php
+++ app/routes/public/api.php
@@ -3,11 +3,22 @@
 /**
  * Staff API
  */
-Route::get('api/(:any)', function() {
+Route::get(array('api', 'api/(:any)'), function($slug = '') {
+//Route::get(array('api/(:any)', 'api/(:any)/(:any)'), function() {
 
-  $fields = array('id', 'display_name', 'position', 'email', 'telephone', 'slug', 'gender');
-  $staffs = Staff::where('status', '=', 'active')->where('grade', '>=', '22')->sort(Base::table('staffs.grade'), 'desc')->get(Staff::fields($fields));
-  $api = array();
+    // get division id
+    $division = !empty($slug) ? Division::slug($slug)->id : '';
+
+    $fields = array('id', 'display_name', 'position', 'email', 'telephone', 'slug', 'gender');
+    $staffs = Staff::where('status', '=', 'active')->where('grade', '>=', '22')->sort(Base::table('staffs.grade'), 'desc');
+
+    if ( !empty($slug) and $division = Division::slug($slug)->id) {
+        $staffs = $staffs->where('division', '=', $division);
+    }
+
+    $staffs = $staffs->get(Staff::fields($fields));
+
+    $api = array();
 
   foreach ($staffs as $key => $staff) {
 
@@ -16,40 +27,40 @@ Route::get('api/(:any)', function() {
     foreach ($extends as $extend) {
       switch($extend->field) {
         case 'text':
-          if( ! empty($extend->value->text)) {
+        if( ! empty($extend->value->text)) {
             $staff->twitter = $extend->value->text;
-          }
-          break;
+        }
+        break;
 
         case 'image':
-          if( ! empty($extend->value->filename)) {
+        if( ! empty($extend->value->filename)) {
             $staff->avatar = $extend->value->filename;
-          }
+        }
 
-          if (!$staff->avatar) {
-              $staff->avatar = ($staff->gender == 'M') ? 'default-male.jpg' : 'default-female.jpg';
-          }
-          break;
+        if (!$staff->avatar) {
+          $staff->avatar = ($staff->gender == 'M') ? 'default-male.jpg' : 'default-female.jpg';
       }
-    }
+      break;
+  }
+}
 
     // convert to accosiative array
-    foreach ($fields as $field) {
-      $api[$key][$field] = $staff->$field;
-      $api[$key]['tokens'][] = $staff->$field;
-      $api[$key]['value'] = $staff->slug;
-      $api[$key]['avatar'] = $staff->avatar;
-      $api[$key]['twitter'] = $staff->twitter;
+foreach ($fields as $field) {
+  $api[$key][$field] = $staff->$field;
+  $api[$key]['tokens'][] = $staff->$field;
+  $api[$key]['value'] = $staff->slug;
+  $api[$key]['avatar'] = $staff->avatar;
+  $api[$key]['twitter'] = $staff->twitter;
       //$api[$key]['token'] = array($staff->display_name, $staff->email);
-    }
-  }
+}
+}
 
   //print_r($api); exit();
 
-  $json = Json::encode($api);
+$json = Json::encode($api);
 
-  return Response::create($json, 200, array(
-        'X-Frame-Options' => 'SAMEORIGIN',
-        'content-type' => 'application/json'
+return Response::create($json, 200, array(
+    'X-Frame-Options' => 'SAMEORIGIN',
+    'content-type' => 'application/json'
     ));
 });
