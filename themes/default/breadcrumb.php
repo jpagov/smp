e17e459aaaabacf1749ab421d6bed10fd827bd23
Hariadi Hinta <diperakui@yahoo.com>
2014-08-14 15:08:30 +0800

    fix breadcrumb for top management

--- themes/default/breadcrumb.php
+++ themes/default/breadcrumb.php
@@ -1,14 +1,15 @@
 <!-- Breadcrumb -->
 <ul class="breadcrumb">
-  <li><a href="#">Home</a></li>
-  <?php if (is_staff()) : ?>
+    <li><a href="#">Home</a></li>
+    <?php if (is_staff()) : ?>
     <li><a href="<?php echo staff_division_url(); ?>"><?php echo staff_division_slug(); ?></a></li>
-  <?php else: ?>
+    <li class="active"><?php echo (staff_name()); ?></li>
+    <?php else: ?>
     <?php if (!is_managementpage()) : ?>
-        <li><a href="<?php echo base_url('divisions'); ?>">Division</a></li>
+    <li><a href="<?php echo base_url('divisions'); ?>">Division</a></li>
+    <li class="active"><?php echo (staff_name()) ? staff_name() : division(); ?></li>
+    <?php else: ?>
+    <li class="active"><?php echo 'Top Management'; ?></li>
     <?php endif; ?>
-  <?php endif; ?>
-  <li class="active"><?php echo (staff_name()) ? staff_name() : is_managementpage() ? 'Top Management' : division(); ?></li>
+    <?php endif; ?>
 </ul>
-
-
