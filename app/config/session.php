2893b2e4ba09328fe476b95534e8e0381e7c90a2
Hariadi Hinta <diperakui@yahoo.com>
2015-01-06 11:22:29 +0800

    Update session cookie

--- app/config/session.php
+++ app/config/session.php
@@ -2,12 +2,12 @@
 
 return array(
 	'driver' => 'database',
-	'cookie' => 'appcms',
+	'cookie' => 'smpapp',
 	'table' => 'ed_sessions',
 	'lifetime' => 86400,
 	//604800 //one week
 	'expire_on_close' => false,
-	'path' => '/',
+	'path' => '/smp',
 	'domain' => '',
 	'secure' => true
 );
