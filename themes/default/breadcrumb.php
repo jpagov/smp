6f03952d05de132b8d29c724ba2614909bf148d1
Hariadi Hinta <diperakui@yahoo.com>
2015-01-07 22:33:05 +0800

    Use latest BreadcrumbListy schema

--- themes/default/breadcrumb.php
+++ themes/default/breadcrumb.php
@@ -1,31 +1,44 @@
 <!-- Breadcrumb -->
 <?php //dd(breadcrumbs()); ?>
-<ul class="breadcrumb">
+<ul class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
 
-    <li itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="<?php echo base_url(); ?>" itemprop="url"><i class="glyphicon glyphicon-home"></i> <span class="sr-only" itemprop="title">Home</span></a></li>
+    <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+
+      <a itemprop="item" href="<?php echo base_url(); ?>"><i class="glyphicon glyphicon-home"></i> <span class="sr-only" itemprop="name">Home</span></a>
+      <meta itemprop="position" content="1" />
+    </li>
 
     <?php if (is_managementpage()) : ?>
 
-    <li class="active" itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><?php echo _e('site.top_management'); ?></li>
+    <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><span itemprop="name"><?php echo _e('site.top_management'); ?></span></li>
 
     <?php else: ?>
 
     <?php if (total_breadcrumb()) :
     $morethan3 = (count(breadcrumbs()) >= 3) ? true: false;
+
+    $position = 2;
     foreach(breadcrumbs() as $key => $org):
     	$title = ($morethan3) ? acronym($org['title']) : $org['title'];
     $active = (((total_breadcrumb() - 1) === array_search($key, array_keys(breadcrumbs())))) ? true : false; ?>
 
     <?php if (is_staff()) : ?>
-    <li itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="<?php echo base_url('division/' . $org['url']); ?>" title="<?php echo $org['title']; ?>"><span itemprop="title"><?php echo $title; ?></span></a></li>
+    <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
 
-		<?php else : ?>
+    	<a itemprop="item" href="<?php echo base_url('division/' . $org['url']); ?>" title="<?php echo $org['title']; ?>"><span itemprop="name"><?php echo $title; ?></span></a>
+    	<meta itemprop="position" content="<?php echo $position; ?>" />
 
-	<li<?php if ($active) { echo ' class="active"'; } else {  echo ' itemscope itemtype="http://data-vocabulary.org/Breadcrumb"'; }; ?>><?php if (!$active) : ?><a href="<?php echo base_url('division/' . $org['url']); ?>" title="<?php echo $org['title']; ?>" itemprop="url"><?php endif; ?><span itemprop="title"><?php echo $title; ?></span><?php if (!$active) : ?></a><?php endif; ?></li>
+    </li>
 
-	<?php endif; ?>
+	<?php else : ?>
 
-	<?php endforeach; ?>
+	<li<?php if ($active) { echo ' class="active"'; } else {  echo ' itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"'; }; ?>><?php if (!$active) : ?>
+
+		<a itemprop="item" href="<?php echo base_url('division/' . $org['url']); ?>" title="<?php echo $org['title']; ?>"><?php endif; ?><span itemprop="name"><?php echo $title; ?></span><?php if (!$active) : ?></a>
+		<meta itemprop="position" content="<?php echo $position; ?>" />
+
+	<?php endif; ?></li>
+	<?php endif; $position++; endforeach; ?>
 
 	<?php if (is_staff()) : ?>
 
@@ -35,7 +48,13 @@
 
 	<?php else : ?>
 
-	<li itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="<?php echo staff_division_url(); ?>"><?php echo staff_division_slug(); ?></a></li>
+	<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+
+		<a itemprop="item" href="<?php echo staff_division_url(); ?>"><span itemprop="name"><?php echo staff_division_slug(); ?></span></a>
+		<meta itemprop="position" content="2" />
+
+	</li>
+
     <li class="active"><?php echo (staff_name()); ?></li>
 
 	<?php endif; ?>
