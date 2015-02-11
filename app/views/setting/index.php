d475d8a0c63356f49a6dcb1af44a8db86c788968
Hariadi Hinta <diperakui@yahoo.com>
2015-02-11 14:25:03 +0800

    view: Extend

--- app/views/setting/index.php
+++ app/views/setting/index.php
@@ -1,38 +1,36 @@
 <?php echo $header; ?>
 
-<hgroup class="wrap">
-	<h1><?php echo __('extend.extend'); ?></h1>
-</hgroup>
+<h1 class="page-header"><?php echo __('extend.extend'); ?></h1>
 
 <section class="wrap">
 	<?php echo $messages; ?>
 
-	<ul class="list">
-		<li>
-			<a href="<?php echo Uri::to('admin/setting/fields'); ?>">
-				<strong><?php echo __('extend.fields'); ?></strong>
-				<span><?php echo __('extend.fields_desc'); ?></span>
-			</a>
-		</li>
-		<li>
-			<a href="<?php echo Uri::to('admin/setting/variables'); ?>">
-				<strong><?php echo __('extend.variables'); ?></strong>
-				<span><?php echo __('extend.variables_desc'); ?></span>
-			</a>
-		</li>
-		<li>
-			<a href="<?php echo Uri::to('admin/setting/metadata'); ?>">
-				<strong><?php echo __('metadata.metadata'); ?></strong>
-				<span><?php echo __('metadata.metadata_desc'); ?></span>
-			</a>
-		</li>
-		<li>
-			<a href="<?php echo Uri::to('admin/setting/plugins'); ?>">
-				<strong>Plugins</strong>
-				<span>Coming soon, yo!</span>
-			</a>
-		</li>
-	</ul>
+	<div class="list-group">
+
+		<a href="<?php echo Uri::to('admin/setting/fields'); ?>" class="list-group-item">
+			<h3 class="list-group-item-heading"><?php echo __('extend.fields'); ?></h3>
+			<p class="list-group-item-text"><?php echo __('extend.fields_desc'); ?></p>
+		</a>
+
+
+		<a href="<?php echo Uri::to('admin/setting/variables'); ?>" class="list-group-item">
+			<h3 class="list-group-item-heading"><?php echo __('extend.variables'); ?></h3>
+			<p class="list-group-item-text"><?php echo __('extend.variables_desc'); ?></p>
+		</a>
+
+
+		<a href="<?php echo Uri::to('admin/setting/metadata'); ?>" class="list-group-item">
+			<h3 class="list-group-item-heading"><?php echo __('metadata.metadata'); ?></h3>
+			<p class="list-group-item-text"><?php echo __('metadata.metadata_desc'); ?></p>
+		</a>
+
+
+		<a href="<?php echo Uri::to('admin/setting/plugins'); ?>" class="list-group-item">
+			<h3 class="list-group-item-heading">Plugins</h3>
+			<p class="list-group-item-text">Coming soon, yo!</p>
+		</a>
+
+	</div>
 </section>
 
 <?php echo $footer; ?>
