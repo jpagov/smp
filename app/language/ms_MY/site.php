ce7a688d91c9ed4ecb9328a7f5b8b694c418e6eb
Hariadi Hinta <diperakui@yahoo.com>
2016-06-30 14:39:55 +0800

    Update UKK

--- app/language/ms_MY/site.php
+++ app/language/ms_MY/site.php
@@ -62,7 +62,7 @@ return array(
 
 	// pkppa
 	'pkppa' => 'Pejabat Ketua Pengarah Perkhidmatan Awam',
-	'paskom' => 'Unit Komunikasi Korporat',
+	'ukk' => 'Unit Komunikasi Korporat',
 	'audit' => 'Unit Audit Dalam',
 	'integriti' => 'Unit Integriti',
 	'law' => 'Penasihat Undang-undang',
