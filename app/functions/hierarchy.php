f0339931e40d68f92504bfe2faec9660f187a7b0
Hariadi Hinta <diperakui@yahoo.com>
2017-02-26 22:14:59 +0800

    Update hierarchies for non active too

--- app/functions/hierarchy.php
+++ app/functions/hierarchy.php
@@ -6,7 +6,7 @@
 
 function total_hierarchies($type = 'branch')
 {
-    if ( ! $hierarchies = Registry::get($type . 's')) {
+    if (! $hierarchies = Registry::get($type . 's')) {
         //$hierarchies = Branch::get();
 
         //$hierarchies = new Items($hierarchies);
@@ -21,7 +21,7 @@ function total_hierarchies($type = 'branch')
 // loop hierarchies
 function hierarchies($type = 'branch')
 {
-    if ( ! total_hierarchies()) {
+    if (! total_hierarchies()) {
         return false;
     }
 
@@ -73,10 +73,13 @@ function hierarchy($id, $type = 'division')
     $valid = array('division', 'branch', 'sector', 'unit');
 
     // if (in_array($type, $valid)) {
-    //     $hierarchies = Hierarchy::where($type, '=', $id)->get();
-    // }
+    //     if ($hierarchies = Hierarchy::where($type, '=', $id)->get()) {
+    //     	$group = array_group_by($hierarchies, 'branch', 'sector', 'unit');
+    //         dd($group[16]);
 
-    $group = $valid[array_search($type, $valid)+1];
+    //     }
+    // }
+    $group = $valid[array_search($type, $valid) + 1];
 
     return in_array($type, $valid)
         ? Hierarchy::where($type, '=', $id)
@@ -96,7 +99,6 @@ function group_by($staffs, $id)
     foreach (hierarchy($id) as $division) {
         foreach (hierarchy($division->branch, 'branch') as $branch) {
             foreach (hierarchy($branch->sector, 'sector') as $sector) {
-
                 foreach ($staffs as $key => $staff) {
                     if ($staff->unit == $sector->unit) {
                         $hierarchies['childs'][$staff->branch_title]['childs'][$staff->sector_title]['childs'][$staff->unit_title]['staffs'][] = $staff;
@@ -106,7 +108,6 @@ function group_by($staffs, $id)
                             return $k === $key;
                         });
                     }
-
                 }
             }
 
@@ -138,7 +139,7 @@ function group_by($staffs, $id)
 
     // listing for direct under divisions, means the director
     foreach ($staffs as $key => $staff) {
-    	$hierarchies['staffs'][] = $staff;
+        $hierarchies['staffs'][] = $staff;
     }
 
     $hierarchies['id'] = $id;
@@ -149,7 +150,7 @@ function group_by($staffs, $id)
 
 function sortBy(array $array)
 {
-    return usort($array, function($a, $b) {
+    return usort($array, function ($a, $b) {
         return strcmp($b->grade, $b->grade);
     });
 }
@@ -157,15 +158,16 @@ function sortBy(array $array)
 function flatten(array $array)
 {
     $return = array();
-    array_walk_recursive($array, function($a,$b) use (&$return) {
+    array_walk_recursive($array, function ($a, $b) use (&$return) {
         $return[$b] = $a;
     });
+
     return $return;
 }
 
 function array_search_key(array $array, $needle)
 {
-    $iterator  = new RecursiveArrayIterator($array);
+    $iterator = new RecursiveArrayIterator($array);
     $recursive = new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::SELF_FIRST);
     foreach ($recursive as $key => $value) {
         if ($key == $needle) {
@@ -174,7 +176,7 @@ function array_search_key(array $array, $needle)
     }
 }
 
-function walk_recursive_remove (array $array, callable $callback)
+function walk_recursive_remove(array $array, callable $callback)
 {
     foreach ($array as $k => $v) {
         if (is_array($v)) {
@@ -189,7 +191,7 @@ function walk_recursive_remove (array $array, callable $callback)
     return $array;
 }
 
-if ( ! function_exists('array_group_by') ) {
+if (! function_exists('array_group_by')) {
     /**
      * Groups an array by a given key.
      *
@@ -211,122 +213,116 @@ if ( ! function_exists('array_group_by') ) {
      *
      * @return array|null Returns a multidimensional array or `null` if `$key` is invalid.
      */
-    function array_group_by( array $array, $key )
-    {
-        if ( ! is_string( $key ) && ! is_int( $key ) && ! is_float( $key ) && ! is_callable( $key ) ) {
-            trigger_error( 'array_group_by(): The key should be a string, an integer, or a callback', E_USER_ERROR );
-            return null;
-        }
-        $func = ( is_callable( $key ) ? $key : null );
-        $_key = $key;
-        // Load the new array, splitting by the target key
-        $grouped = [];
-        foreach ( $array as $value ) {
-            if ( is_callable( $func ) ) {
-                $key = call_user_func( $func, $value );
-            } elseif ( is_object( $value ) && isset( $value->{ $_key } ) ) {
-                $key = $value->{ $_key };
-            } elseif ( isset( $value[ $_key ] ) ) {
-                $key = $value[ $_key ];
-            } else {
-                continue;
+        function array_group_by(array $array, $key)
+        {
+            if (!is_string($key) && !is_int($key) && !is_float($key) && !is_callable($key)) {
+                trigger_error('array_group_by(): The key should be a string, an integer, or a callback', E_USER_ERROR);
+
+                return null;
             }
-            $grouped[ $key ][] = $value;
-        }
+            $func = (is_callable($key) ? $key : null);
+            $_key = $key;
+
+            $grouped = [];
+            foreach ($array as $value) {
+                $grouped[$value->{$_key}][] = $value;
+            }
+
         // Recursively build a nested grouping if more parameters are supplied
         // Each grouped array value is grouped according to the next sequential key
-        if ( func_num_args() > 2 ) {
+        if (func_num_args() > 2) {
             $args = func_get_args();
-            foreach ( $grouped as $key => $value ) {
-                $params = array_merge( [ $value ], array_slice( $args, 2, func_num_args() ) );
-                $grouped[ $key ] = call_user_func_array( 'array_group_by', $params );
+            foreach ($grouped as $key => $value) {
+                $params = array_merge([ $value ], array_slice($args, 2, func_num_args()));
+                $grouped[$key] = call_user_func_array('array_group_by', $params);
             }
         }
-        return $grouped;
-    }
+
+            return $grouped;
+        }
 }
 
-function htmlOrg($orgs, $level = 1, $collapsing = true) {
+function htmlOrg($orgs, $level = 1, $collapsing = true)
+{
+    $htmlOrg = '';
+    $collapse = $collapsing ? 'in' : '';
+    $expanded = $collapsing ? 'true' : 'false';
 
-	$htmlOrg = '';
-	$collapse = $collapsing ? 'in' : '';
-	$expanded = $collapsing ? 'true' : 'false';
+    if (! isset($orgs['childs'])) {
+        return;
+    }
 
-	if (! isset($orgs['childs'])) {
-		return;
-	}
+    foreach ($orgs['childs'] as $itemsKey => $org) :
 
-		foreach ($orgs['childs'] as $itemsKey => $org) :
+            $index = isset($org['type']) ? $org['type']. '-' . $org['id'] : '';
 
-			$index = isset($org['type']) ? $org['type']. '-' . $org['id'] : '';
+    $htmlOrg .= '<div class="panel panel-primary">';
+    $htmlOrg .= '<!-- Default panel contents -->';
+    $htmlOrg .= '<div class="panel-heading" role="tab" id="heading-' . $index . '">';
+    $htmlOrg .= '<h4 class="panel-title">';
 
-			$htmlOrg .= '<div class="panel panel-primary">';
-			$htmlOrg .= '<!-- Default panel contents -->';
-			$htmlOrg .= '<div class="panel-heading" role="tab" id="heading-' . $index . '">';
-			$htmlOrg .= '<h4 class="panel-title">';
+    $htmlOrg .= isset($org['childs']) ? '<a class="accordion-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-' . $index . '" aria-expanded="'. $expanded  .'" aria-controls="collapse-' . $index . '">' . $itemsKey . '</a>' : $itemsKey;
 
-			$htmlOrg .= isset($org['childs']) ? '<a class="accordion-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-' . $index . '" aria-expanded="'. $expanded  .'" aria-controls="collapse-' . $index . '">' . $itemsKey . '</a>' : $itemsKey;
+    $htmlOrg .= '</h4>';
 
-			$htmlOrg .= '</h4>';
+    $htmlOrg .= '</div>';
 
-			$htmlOrg .= '</div>';
+    $htmlOrg .= '<table class="table">';
 
-			$htmlOrg .= '<table class="table">';
+    $htmlOrg .= '<thead>';
+    $htmlOrg .= '<tr>';
+    $htmlOrg .= '<th width="30%">Nama</th>';
+    $htmlOrg .= '<th data-toggle="tooltip" title="Jawatan"><i class="glyphicon glyphicon-barcode"></i> Jawatan</th>';
+    $htmlOrg .= '<th data-toggle="tooltip" title="Emel"><i class="glyphicon glyphicon-envelope"></i> Emel</th>';
+    $htmlOrg .= '<th data-toggle="tooltip" title="Telefon"><i class="glyphicon glyphicon-phone-alt"></i> Telefon</th>';
+    $htmlOrg .= '</tr>';
+    $htmlOrg .= '</thead>';
+    $htmlOrg .= '<tbody>';
 
-			$htmlOrg .= '<thead>';
-			$htmlOrg .= '<tr>';
-			$htmlOrg .= '<th width="30%">Nama</th>';
-			$htmlOrg .= '<th data-toggle="tooltip" title="Jawatan"><i class="glyphicon glyphicon-barcode"></i> Jawatan</th>';
-			$htmlOrg .= '<th data-toggle="tooltip" title="Emel"><i class="glyphicon glyphicon-envelope"></i> Emel</th>';
-			$htmlOrg .= '<th data-toggle="tooltip" title="Telefon"><i class="glyphicon glyphicon-phone-alt"></i> Telefon</th>';
-			$htmlOrg .= '</tr>';
-			$htmlOrg .= '</thead>';
-			$htmlOrg .= '<tbody>';
+    if (isset($org['staffs'])) :
+                foreach ($org['staffs'] as $staff) :
 
-			if (isset($org['staffs'])) :
-				foreach ($org['staffs'] as $staff) :
+                    $htmlOrg .= '<tr>';
 
-					$htmlOrg .= '<tr>';
+    $htmlOrg .= '<td><a class="staff-ajax" ' . (site_meta('enable_staff_modal', true) ? 'data-toggle="modal"' : '') . ' href="' . base_url(Page::staff() . '/' . $staff->slug) . '" data-target="#staffModal">' . $staff->display_name . '</a></td>';
 
-			        $htmlOrg .= '<td><a class="staff-ajax" ' . (site_meta('enable_staff_modal', true) ? 'data-toggle="modal"' : '' ) . ' href="' . base_url(Page::staff() . '/' . $staff->slug) . '" data-target="#staffModal">' . $staff->display_name . '</a></td>';
+    $htmlOrg .= '<td>' . $staff->job_title . '</td>';
 
-			        $htmlOrg .= '<td>' . $staff->job_title . '</td>';
+    $htmlOrg .= '<td>';
+    if ($staff->email) :
+                        //$htmlOrg .= '<a href="mailto:' . Encode::email($staff->email) . '">';
+                        $htmlOrg .= '<span><img src="' . Encode::email2image($staff->email) . '" alt="'. $staff->display_name .'"></span>';
+                        //$htmlOrg .= '</a>';
+                    else :
+                        $htmlOrg .= __('site.na');
+    endif;
+    $htmlOrg .= '</td>';
+    $htmlOrg .= '<td>' . (site_meta('short_phone', false) ? str_replace(site_meta('short_phone', array('03-8885', '03 8885')), '', $staff->telephone) : $staff->telephone) . '</td>';
+    $htmlOrg .= '</tr>';
 
-			        $htmlOrg .= '<td>';
-			        if ($staff->email) :
-			        	//$htmlOrg .= '<a href="mailto:' . Encode::email($staff->email) . '">';
-			        	$htmlOrg .= '<span><img src="' . Encode::email2image($staff->email) . '" alt="'. $staff->display_name .'"></span>';
-			        	//$htmlOrg .= '</a>';
-			        else :
-			        	$htmlOrg .= __('site.na');
-			        endif;
-			        $htmlOrg .= '</td>';
-			        $htmlOrg .= '<td>' . ( site_meta('short_phone', false) ? str_replace(site_meta('short_phone', array('03-8885', '03 8885')), '', $staff->telephone) : $staff->telephone ) . '</td>';
-			    	$htmlOrg .= '</tr>';
+    endforeach;
+    endif;
 
-				endforeach;
-			endif;
+    $htmlOrg .= '</tbody>';
+    $htmlOrg .= '</table>';
 
-			$htmlOrg .= '</tbody>';
-			$htmlOrg .= '</table>';
+    $htmlOrg .= '</div>';
+    $htmlOrg .= '<hr>';
 
-			$htmlOrg .= '</div>';
-			$htmlOrg .= '<hr>';
+    if (isset($org['childs'])) :
 
-			if (isset($org['childs'])) :
+                $htmlOrg .= ($collapsing) ? '<div id="collapse-' . $index . '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-' . $index . '">' : '';
 
-				$htmlOrg .= ($collapsing) ? '<div id="collapse-' . $index . '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-' . $index . '">' : '';
+    $htmlOrg .= htmlOrg($org, $level++);
 
-				$htmlOrg .= htmlOrg($org, $level++);
+    $htmlOrg .= ($collapsing) ? '</div>' : '';
 
-				$htmlOrg .= ($collapsing) ? '</div>' : '';
-
-			endif;
+    endif;
 
 
-			$level++;
+    $level++;
 
-		endforeach;
+    endforeach;
 
-	return $htmlOrg;
+    return $htmlOrg;
 }
