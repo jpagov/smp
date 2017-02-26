b5171ba6a4f4e417fcd3ac40cfbc6cfb4e57e890
Hariadi Hinta <diperakui@yahoo.com>
2017-02-26 15:52:49 +0800

    ask if we want to update hierarchies

--- src/Smp/Console/Command/HierarchyCommand.php
+++ src/Smp/Console/Command/HierarchyCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Smp\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
@@ -8,74 +9,66 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Helper\ProgressBar;
 
+class HierarchyCommand extends Command
+{
+    protected $valids = ['pkppa', 'pkppap', 'pkppao', 'bkp', 'bppd', 'intan', 'bpms', 'bpps', 'bs', 'bp', 'bk', 'bmi', 'bpo', 'btsa'];
+    protected $divisions = [];
 
-class HierarchyCommand extends Command {
-	protected function configure() {
-		$this->setName("hierarchy")
-			 ->setDescription("Update all staff hierarchy")
-			 ->addArgument(
-				'division',
-				InputArgument::IS_ARRAY,
-				'Filter by division for updating hierarchy. Can use division id or slug. Delimited by space'
-			 )
-			 ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force to update all staff. Staff that have null in division, branch, sector, and unit will not update.' . PHP_EOL)
-			 ->setHelp('Update <info>hierarchy</info> for all staff');
-	}
+    protected function configure()
+    {
+        $this->setName("hierarchy")
+             ->setDescription("Update all staff hierarchy")
+             ->addArgument(
+                'division',
+                InputArgument::IS_ARRAY,
+                'Filter by division for updating hierarchy (Delimited by space)',
+                $this->valids
+             )
+             ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force to update all staff. Staff that have null in division, branch, sector, and unit will not update.' . PHP_EOL)
+             ->setHelp('Update <info>hierarchy</info> for all staff');
+    }
 
-	protected function execute(InputInterface $input, OutputInterface $output) {
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+    	$formatter = $this->getHelper('formatter');
+        $filter = false;
+        $divisions = \Division::listing();
 
-		$filter = false;
-		$defaults = \Division::listing();
-		$id = array();
+        foreach ($divisions as $division) {
+            $this->divisions[$division->slug] = $division->id;
+        }
 
-		if ($input = $input->getArgument('division')) {
-			$defaults = $input;
-			$filter = true;
+        $valid = array_intersect($this->valids, $input->getArgument('division'));
 
-			foreach ($defaults as $div) {
+        $ids = array_intersect_key($this->divisions, array_flip($valid));
 
-				if (!ctype_digit($div)) {
-					if (!$division = \Division::slug($div)) {
-						$output->writeln('<error>No division found!</error>');
-						return;
-					}
-				}
+        $query = \Staff::where('status', '=', 'active')->where_in('division', array_values($ids) , 'AND ');
 
-				$id[] = $division->id;
-			}
-		}
+        $count = $query->count();
+        $staffs = $query->get();
 
-		$query = \Staff::where('status', '=', 'active')
-			->where('division', '!=', 0)
-			->where('branch', '!=', 0)
-			->where('sector', '!=', 0)
-			->where('unit', '!=', 0);
+        $progress = new ProgressBar($output, 50);
+        $progress->start();
 
-		if ($filter) {
-			$query->where_in('division', $id, 'AND ');
-		}
+        foreach ($staffs as $staff) {
+        	$formattedLine = $formatter->formatSection(
+                strtoupper(array_search($staff->division, $this->divisions)),
+                '<comment>Updating</comment>: ' . $staff->display_name
+            );
+        	$output->writeln($formattedLine);
+            \Hierarchy::where('staff', '=', $staff->id)->delete();
+            \Hierarchy::create(array(
+                'staff' => $staff->id,
+                'division' => ($staff->division) ?: 0,
+                'branch' => ($staff->branch) ?: 0,
+                'sector' => ($staff->sector) ?: 0,
+                'unit' => ($staff->unit) ?: 0,
+            ));
+            $progress->advance();
+        }
 
-		$count = $query->count();
-		$staffs = $query->get();
+        $progress->finish();
 
-		$progress = new ProgressBar($output, 50);
-		$progress->start();
-
-		foreach ($staffs as $staff) {
-
-			\Hierarchy::where('staff', '=', $staff->id)->delete();
-			\Hierarchy::create(array(
-				'staff' => $staff->id,
-				'division' => ($staff->division) ?: 0,
-				'branch' => ($staff->branch) ?: 0,
-				'sector' => ($staff->sector) ?: 0,
-				'unit' => ($staff->unit) ?: 0,
-			));
-			$progress->advance();
-		}
-
-		$progress->finish();
-
-		$output->writeln(PHP_EOL . PHP_EOL . 'Done updating ' . $count . ' staff');
-	}
+        $output->writeln(PHP_EOL . PHP_EOL . 'Done updating ' . $count . ' staff');
+    }
 }
