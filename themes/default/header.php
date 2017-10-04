6380c0d8d360051aac526cb6f43d2917a2dcf0b3
Hariadi Hinta <diperakui@yahoo.com>
2017-10-04 11:02:40 +0800

    Use coat of arms of Malaysia

--- themes/default/header.php
+++ themes/default/header.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php if (base_url() == current_url()) : echo site_name(); else : echo page_title('Page can’t be found'); ?> - <?php echo site_name(); ?><?php endif; ?> ☏</title>
+  <link rel="shortcut icon" type="image/png" href="<?php echo base_url('favicon.ico?=04102017'); ?>"/>
   <meta name="description" content="<?php echo staff_description_text(); ?>">
   <?php theme_include('critical'); ?>
   <link rel="stylesheet" href="<?php echo revision('css/app.min.css'); ?>">
