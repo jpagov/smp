71f10951628d16032d84900510abdb7b4c28e7ce
Hariadi Hinta <diperakui@yahoo.com>
2018-07-03 11:02:20 +0800

    Fix tranfer when no editor for department

--- app/models/transfer.php
+++ app/models/transfer.php
@@ -15,23 +15,29 @@ class Transfer extends Base
         return $items;
     }
 
-    public static function expired() {
-    	$table = Base::table('transfers');
-    	$sql = 'SELECT * FROM `'. $table . '` WHERE NOW() > DATE_ADD(`transfered_at`, INTERVAL ' . Config::meta('custom_transfer_days_expired', 7) . ' DAY)';
+    public static function expired()
+    {
+        $table = Base::table('transfers');
+        $sql = 'SELECT * FROM `'. $table . '` WHERE NOW() > DATE_ADD(`transfered_at`, INTERVAL ' . Config::meta('custom_transfer_days_expired', 7) . ' DAY)';
 
-    	list($result, $statement) = DB::ask($sql);
+        list($result, $statement) = DB::ask($sql);
 
-    	if ($result) {
-    		$transfers = $statement->fetchAll(PDO::FETCH_OBJ);
+        if ($result) {
+            $transfers = $statement->fetchAll(PDO::FETCH_OBJ);
 
-	    	foreach ($transfers as $transfer) {
-	    		static::update($transfer->id, [
-		    		'confirmed_at' => Date::mysql('now'),
-		    		'confirmed_by' => Staff::editors($transfer->transfer_to)[0]->id,
-		    		'status' => 'cancel'
-		    	]);
-	    	}
-    	}
+            foreach ($transfers as $transfer) {
+                $editors = Staff::editors($transfer->transfer_to);
+                if (empty($editors)) {
+                    continue;
+                }
+
+                static::update($transfer->id, [
+                    'confirmed_at' => Date::mysql('now'),
+                    'confirmed_by' => $editors[0]->id,
+                    'status' => 'cancel'
+                ]);
+            }
+        }
     }
 
     public static function all($columns = null)
@@ -56,7 +62,7 @@ class Transfer extends Base
         $query = $query->where(Base::table('transfers.'. $type), '=', $id);
 
         if ($confirmed_at) {
-        	$query = $query->where(Base::table('transfers.confirmed_at'), 'is', null);
+            $query = $query->where(Base::table('transfers.confirmed_at'), 'is', null);
         }
 
         $results = $query->sort(Base::table('transfers.transfered_at'), 'desc')
@@ -67,22 +73,22 @@ class Transfer extends Base
 
     public static function paginate($page = 1, $perpage = 10, $search = null, $status = null)
     {
-    	$editor = Auth::user();
+        $editor = Auth::user();
 
         $query = static::buildQuery();
 
         if ($search) {
-        	$query = $query->where('s.display_name', 'like', '%' . $search['term'] . '%');
+            $query = $query->where('s.display_name', 'like', '%' . $search['term'] . '%');
         }
 
         if ($status) {
-        	$query = $query->where(Base::table('transfers.status'), '=', $status);
+            $query = $query->where(Base::table('transfers.status'), '=', $status);
         }
 
         $query = $query
-        	->where_in(Base::table('transfers.transfer_to'), $editor->roles, 'AND ')
-        	->where_in(Base::table('transfers.transfer_from'), $editor->roles)
-        	->where(Base::table('transfers.status'), '=', 'transfer');
+            ->where_in(Base::table('transfers.transfer_to'), $editor->roles, 'AND ')
+            ->where_in(Base::table('transfers.transfer_from'), $editor->roles)
+            ->where(Base::table('transfers.status'), '=', 'transfer');
 
         $count = $query->count();
 
@@ -95,35 +101,45 @@ class Transfer extends Base
     {
         return Query::table(static::table())->left_join(
             Base::table('staffs as s'),
-            's.id', '=', Base::table('transfers.staff_id')
+            's.id',
+            '=',
+            Base::table('transfers.staff_id')
         )->left_join(
             Base::table('staffs as a'),
-            'a.id', '=', Base::table('transfers.transfer_by')
+            'a.id',
+            '=',
+            Base::table('transfers.transfer_by')
         )->left_join(
             Base::table('staffs as b'),
-            'b.id', '=', Base::table('transfers.confirmed_by')
+            'b.id',
+            '=',
+            Base::table('transfers.confirmed_by')
         )->left_join(
             Base::table('divisions as from'),
-            'from.id', '=', Base::table('transfers.transfer_from')
+            'from.id',
+            '=',
+            Base::table('transfers.transfer_from')
         )->left_join(
             Base::table('divisions as to'),
-            'to.id', '=', Base::table('transfers.transfer_to')
+            'to.id',
+            '=',
+            Base::table('transfers.transfer_to')
         );
     }
 
     protected static function buildSelect()
     {
         return [
-        	Base::table('transfers.*'),
-        	's.display_name as staff',
-        	'a.display_name as request_by',
-        	'b.display_name as confirm_by',
-        	'from.id as division_from_id',
-        	'from.title as division_from',
-        	'from.slug as division_from_slug',
-			'to.id as division_to_id',
-        	'to.title as division_to',
-        	'to.slug as division_to_slug',
+            Base::table('transfers.*'),
+            's.display_name as staff',
+            'a.display_name as request_by',
+            'b.display_name as confirm_by',
+            'from.id as division_from_id',
+            'from.title as division_from',
+            'from.slug as division_from_slug',
+            'to.id as division_to_id',
+            'to.title as division_to',
+            'to.slug as division_to_slug',
         ];
     }
 }
