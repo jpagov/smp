9e1047f3632c60ceab3200c0e801f08ff009f546
Hariadi Hinta <diperakui@yahoo.com>
2018-10-22 10:25:04 +0800

    Use custom meta

--- app/migrations/154_cidrs.php
+++ app/migrations/154_cidrs.php
@@ -7,9 +7,9 @@ class Migration_cidrs extends Migration
         $table = Base::table('meta');
 
         if ($this->has_table($table)) {
-            if (! Query::table($table)->where('key', '=', 'cidrs')->count()) {
+            if (! Query::table($table)->where('key', '=', 'custom_kppa')->count()) {
                 Query::table($table)->insert(array(
-                    'key' => 'cidrs',
+                    'key' => 'custom_kppa',
                     'value' => '10.21.0.0/16,10.217.24.0/24,10.188.2.0/24'
                 ));
             }
